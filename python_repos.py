e4581b4d8aaf1686edadc5b435e394e45075de7e
stillnesslx <wqzqrxl@163.com>
2018-04-01 20:17:05 +0800

    20180401

--- python_repos.py
+++ python_repos.py
@@ -10,7 +10,23 @@ print("Total repositories:", response_dict['total_count'])
 repo_dicts = response_dict['items']
 print("Repositories returned:",len(repo_dicts))
 
-repo_dict = repo_dicts[0]
-print("\nKeys:",len(repo_dict))
-for key in sorted(repo_dict.keys()):
-    print(key)
+print("\nSelected information about each repository:")
+for repo_dict in repo_dicts:
+    print('\nName:',repo_dict['name'])
+    print('Owner:',repo_dict['owner']['login'])
+    print('Stars:', repo_dict['stargazers_count'])
+    print('Repository:',repo_dict['html_url'])
+    print('Descripton:',repo_dict['description'])
+#repo_dict = repo_dicts[0]
+#print("\nKeys:",len(repo_dict))
+#for key in sorted(repo_dict.keys()):
+#    print(key)
+
+#print("\nSelected information about first repository:")
+#print('Name:',repo_dict['name'])
+#print('Owner:',repo_dict['owner']['login'])
+#print('Stars:',repo_dict['stargazers_count'])
+#print('Repository:',repo_dict['html_url'])
+#print('Created:',repo_dict['created_at'])
+#print('Updated:',repo_dict['updated_at'])
+#print('Description:',repo_dict['description'])
